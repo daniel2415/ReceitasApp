397bd83f375df8ecc3e8b45f596e64533115c4f1
Moises <moises@rcbdesenvolvimento.com.br>
2018-05-08 07:55:36 -0300

    alteração cores do app

--- app/src/main/java/br/com/olimpiodev/receitasapp/CookieActivity.java
+++ app/src/main/java/br/com/olimpiodev/receitasapp/CookieActivity.java
@@ -9,5 +9,7 @@ public class CookieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cookie);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
--- app/src/main/java/br/com/olimpiodev/receitasapp/SorveteActivity.java
+++ app/src/main/java/br/com/olimpiodev/receitasapp/SorveteActivity.java
@@ -9,5 +9,7 @@ public class SorveteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sorvete);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }

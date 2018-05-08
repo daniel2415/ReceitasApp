e2956ce3c6254a17eaeebcf96ecff98a942d1b72
Moises <moises@rcbdesenvolvimento.com.br>
2018-05-08 07:43:32 -0300

    setando click dos buttons das receitas

--- app/src/main/java/br/com/olimpiodev/receitasapp/MainActivity.java
+++ app/src/main/java/br/com/olimpiodev/receitasapp/MainActivity.java
@@ -1,7 +1,10 @@
 package br.com.olimpiodev.receitasapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button botaoBolo = findViewById(R.id.botao_bolo);
+        botaoBolo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentBolo = new Intent(MainActivity.this, BoloActivity.class);
+                startActivity(intentBolo);
+            }
+        });
+
+        Button botaoTorta = findViewById(R.id.botao_torta);
+        botaoTorta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentTorta = new Intent(MainActivity.this, TortaActivity.class);
+                startActivity(intentTorta);
+            }
+        });
+
+        Button botaoCookie = findViewById(R.id.botao_cookie);
+        botaoCookie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentCookie = new Intent(MainActivity.this, CookieActivity.class);
+                startActivity(intentCookie);
+            }
+        });
+
+        Button botaoSorvete = findViewById(R.id.botao_sorvete);
+        botaoSorvete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentSorvete = new Intent(MainActivity.this, SorveteActivity.class);
+                startActivity(intentSorvete);
+            }
+        });
     }
 }
